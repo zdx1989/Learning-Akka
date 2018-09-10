d94d8eca4e8e0bc62ffe07e822fdb9b02d77cb3c
zdx1989 <18565653302@163.com>
2018-09-10 23:58:53 +0800

    akka router

---
+++ ch5/akkademaid-scala/src/test/scala/com/akkademy/BalancingPoolSpec.scala
@@ -0,0 +1,10 @@
+package com.akkademy
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by zhoudunxiong on 2018/9/10.
+  */
+class BalancingPoolSpec extends FlatSpec with Matchers {
+
+}
